c58aa38699180c90e0e68be93a0af5dee2784ca1
Vadim <mail@vadimklimenko.com>
2015-01-03 23:22:32 +0200

    Adding current week (numerator/denumerator) num to API

--- timetable/api.py
+++ timetable/api.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from tastypie import fields
-from tastypie.resources import ModelResource, ALL, ALL_WITH_RELATIONS
+from tastypie.resources import Resource, ModelResource, ALL, ALL_WITH_RELATIONS
 from timetable.models import Departament, Group, Teacher, Campus, Audience, Lesson, Timetable
 
 
@@ -68,3 +68,44 @@ class TimetableResource(ModelResource):
         bundle.data['group_departament_name'] = bundle.obj.group.departament.name
 
         return bundle
+
+
+class dictToObj(object):
+    """
+    Convert dictionary to object
+    @source http://stackoverflow.com/a/1305561/383912
+    """
+
+    def __init__(self, d):
+        self.__dict__['d'] = d
+
+    def __getattr__(self, key):
+        value = self.__dict__['d'][key]
+        if type(value) == type({}):
+            return dictToObj(value)
+
+        return value
+
+
+class CurrentWeekResource(Resource):
+    week = fields.CharField(attribute='week')
+
+    class Meta:
+        resource_name = 'current_week'
+        include_resource_uri = False
+
+    def obj_get_list(self, request=None, **kwargs):
+        from datetime import datetime
+
+        bundle = []
+        current_week = (int(datetime.today().strftime("%U")) % 2) + 1 # If first week of year is numerator
+        bundle.append(dictToObj({'week': current_week}))
+        return bundle
+
+    def alter_list_data_to_serialize(self, request, data_dict):
+        if isinstance(data_dict, dict):
+            if 'meta' in data_dict:
+                del (data_dict['meta'])
+            if 'objects' in data_dict:
+                data_dict = data_dict['objects'][0]
+            return data_dict
--- timetable/urls.py
+++ timetable/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import patterns, url, include
 from django.views import generic
 from tastypie.api import Api
 
-from timetable.api import DepartamentResource, GroupResource, TeacherResource, CampusResource, AudienceResource, LessonResource, TimetableResource
+from timetable.api import DepartamentResource, GroupResource, TeacherResource, CampusResource, AudienceResource, \
+    LessonResource, TimetableResource, CurrentWeekResource
 
 v1_api = Api(api_name='v1')
 v1_api.register(DepartamentResource())
@@ -12,8 +13,9 @@ v1_api.register(CampusResource())
 v1_api.register(AudienceResource())
 v1_api.register(LessonResource())
 v1_api.register(TimetableResource())
+v1_api.register(CurrentWeekResource())
 
 urlpatterns = patterns('',
-    # url(r'^$', views.IndexView.as_view(), name='index'),
-    (r'^api/', include(v1_api.urls)),
+                       # url(r'^$', views.IndexView.as_view(), name='index'),
+                       (r'^api/', include(v1_api.urls)),
 )
