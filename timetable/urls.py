07c32da13baf43522945fbe4ec1de6729136d637
Vadim <mail@vadimklimenko.com>
2015-01-09 14:34:08 +0200

    Make available TimeResourse in API urls

--- timetable/urls.py
+++ timetable/urls.py
@@ -3,7 +3,7 @@ from django.views import generic
 from tastypie.api import Api
 
 from timetable.api import DepartmentResource, GroupResource, TeacherResource, CampusResource, AudienceResource, \
-    LessonResource, TimetableResource, CurrentWeekResource
+    LessonResource, TimetableResource, CurrentWeekResource, TimeResource
 
 v1_api = Api(api_name='v1')
 v1_api.register(DepartmentResource())
@@ -12,6 +12,7 @@ v1_api.register(TeacherResource())
 v1_api.register(CampusResource())
 v1_api.register(AudienceResource())
 v1_api.register(LessonResource())
+v1_api.register(TimeResource())
 v1_api.register(TimetableResource())
 v1_api.register(CurrentWeekResource())
 
