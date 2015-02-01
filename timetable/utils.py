09cc1b3c043f0946b6b8cdd3c2c379da81912b50
Vadim <mail@vadimklimenko.com>
2015-02-01 16:35:33 +0200

    Fix datetime

--- timetable/utils.py
+++ timetable/utils.py
@@ -1,8 +1,10 @@
-from datetime import datetime
+import datetime
 
 
 def get_current_week(numerator=None):
-    current_week = (int(datetime.today().strftime("%U")) % 2) + 1  # If first week of year is numerator
+    # from datetime import datetime
+
+    current_week = (int(datetime.datetime.today().strftime("%U")) % 2) + 1  # If first week of year is numerator
     if numerator:
         weeks_order = [1, 2]
         if current_week == numerator:
