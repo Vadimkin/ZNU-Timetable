f4326b0f26036ac85c034b2b257ff25d0258b542
Vadim <mail@vadimklimenko.com>
2015-02-08 18:36:05 +0200

    Show timetable of new week if today — sunday

--- timetable/templatetags/readable_month_day.py
+++ timetable/templatetags/readable_month_day.py
@@ -1,12 +1,17 @@
 import datetime
 from django import template
 from django.utils import dateformat
+from timetable.utils import get_current_week
 from znu import settings
 
 register = template.Library()
 
+
 @register.simple_tag
 def readable_month_day(lesson):
+    """Modify week on sunday. At site if current day = sunday then showing timetable from new week """
+    week = lesson.week if lesson.week == get_current_week() and datetime.datetime.today().weekday() == 6 else lesson.week + 2
+
     today = datetime.date.today()
-    day = today - datetime.timedelta(days=today.weekday()) + datetime.timedelta(days=lesson.day + (lesson.week-1)*7)
+    day = today - datetime.timedelta(days=today.weekday()) + datetime.timedelta(days=lesson.day + (week - 1) * 7)
     return dateformat.format(day, settings.DATE_FORMAT)
--- timetable/utils.py
+++ timetable/utils.py
@@ -1,16 +1,21 @@
 import datetime
 
 
-def get_current_week(numerator=None):
-    # from datetime import datetime
+def get_current_week(numerator=None, auto_week_change=False):
+    if auto_week_change:
+        strftime_param = "%W"
+    else:
+        strftime_param = "%U"
 
-    current_week = (int(datetime.datetime.today().strftime("%U")) % 2)  # If first week of year is numerator
+    current_week = ((int(datetime.datetime.today().strftime(strftime_param)) + 1) % 2) + 1 # If first week of year is numerator
+
     if numerator:
         weeks_order = [1, 2]
+
         if current_week == numerator:
             return weeks_order[0]
         else:
-            return weeks_order[::-1][0]
+            return weeks_order[1]
     else:
         return current_week
 
