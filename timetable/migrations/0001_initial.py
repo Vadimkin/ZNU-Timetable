9720953ffd311b70704e0aa53f955977292a39f5
Vadim <mail@vadimklimenko.com>
2015-03-07 15:06:55 +0200

    Update migrations

--- timetable/migrations/0001_initial.py
+++ timetable/migrations/0001_initial.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+import datetime
 
 
 class Migration(migrations.Migration):
@@ -15,9 +16,10 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('audience', models.TextField(verbose_name=b'\xd0\x90\xd1\x83\xd0\xb4\xd0\xb8\xd1\x82\xd0\xbe\xd1\x80\xd1\x96\xd1\x8f')),
-                ('last_update', models.IntegerField(default=1420801485.409108)),
+                ('last_update', models.IntegerField(default=1425733235)),
             ],
             options={
+                'ordering': ['campus', 'audience'],
                 'verbose_name': '\u0430\u0443\u0434\u0438\u0442\u043e\u0440\u0456\u044f',
                 'verbose_name_plural': '\u0430\u0443\u0434\u0438\u0442\u043e\u0440\u0456\u0457',
             },
@@ -28,7 +30,9 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.TextField(default=b'', verbose_name=b'\xd0\x9a\xd0\xbe\xd1\x80\xd0\xbf\xd1\x83\xd1\x81', blank=True)),
-                ('last_update', models.IntegerField(default=1420801485.408446)),
+                ('last_update', models.IntegerField(default=1425733235)),
+                ('longitude', models.FloatField(null=True, verbose_name=b'Longitude', blank=True)),
+                ('latitude', models.FloatField(null=True, verbose_name=b'Latitude', blank=True)),
             ],
             options={
                 'verbose_name': '\u043a\u043e\u0440\u043f\u0443\u0441',
@@ -41,7 +45,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=500, verbose_name=b'\xd0\xa4\xd0\xb0\xd0\xba\xd1\x83\xd0\xbb\xd1\x8c\xd1\x82\xd0\xb5\xd1\x82')),
-                ('last_update', models.IntegerField(default=1420801485.406066)),
+                ('last_update', models.IntegerField(default=1425733235)),
             ],
             options={
                 'verbose_name': '\u0444\u0430\u043a\u0443\u043b\u044c\u0442\u0435\u0442',
@@ -54,10 +58,12 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=500, verbose_name=b'\xd0\x9d\xd0\xbe\xd0\xbc\xd0\xb5\xd1\x80 \xd0\xb3\xd1\x80\xd1\x83\xd0\xbf\xd0\xb8')),
-                ('last_update', models.IntegerField(default=1420801485.40712)),
+                ('course', models.IntegerField(default=1, choices=[(1, b'1 \xd0\xba\xd1\x83\xd1\x80\xd1\x81'), (2, b'2 \xd0\xba\xd1\x83\xd1\x80\xd1\x81'), (3, b'3 \xd0\xba\xd1\x83\xd1\x80\xd1\x81'), (4, b'4 \xd0\xba\xd1\x83\xd1\x80\xd1\x81'), (5, b'5 \xd0\xba\xd1\x83\xd1\x80\xd1\x81')])),
+                ('last_update', models.IntegerField(default=1425733235)),
                 ('department', models.ForeignKey(verbose_name=b'\xd0\xa4\xd0\xb0\xd0\xba\xd1\x83\xd0\xbb\xd1\x8c\xd1\x82\xd0\xb5\xd1\x82', to='timetable.Department')),
             ],
             options={
+                'ordering': ['course'],
                 'verbose_name': '\u0433\u0440\u0443\u043f\u0430',
                 'verbose_name_plural': '\u0433\u0440\u0443\u043f\u0438',
             },
@@ -68,9 +74,10 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=500, verbose_name=b'\xd0\x9f\xd1\x80\xd0\xb5\xd0\xb4\xd0\xbc\xd0\xb5\xd1\x82')),
-                ('last_update', models.IntegerField(default=1420801485.409798)),
+                ('last_update', models.IntegerField(default=1425733235)),
             ],
             options={
+                'ordering': ['name'],
                 'verbose_name': '\u043f\u0440\u0435\u0434\u043c\u0435\u0442',
                 'verbose_name_plural': '\u043f\u0440\u0435\u0434\u043c\u0435\u0442\u0438',
             },
@@ -81,9 +88,10 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.TextField(verbose_name=b"\xd0\x86\xd0\xbc'\xd1\x8f \xd1\x82\xd0\xb0 \xd0\xbf\xd1\x80\xd1\x96\xd0\xb7\xd0\xb2\xd0\xb8\xd1\x89\xd0\xb5 \xd0\xb2\xd0\xb8\xd0\xba\xd0\xbb\xd0\xb0\xd0\xb4\xd0\xb0\xd1\x87\xd0\xb0")),
-                ('last_update', models.IntegerField(default=1420801485.407833)),
+                ('last_update', models.IntegerField(default=1425733235)),
             ],
             options={
+                'ordering': ['name'],
                 'verbose_name': '\u0432\u0438\u043a\u043b\u0430\u0434\u0430\u0447',
                 'verbose_name_plural': '\u0432\u0438\u043a\u043b\u0430\u0434\u0430\u0447\u0456',
             },
@@ -99,7 +107,7 @@ class Migration(migrations.Migration):
             ],
             options={
                 'verbose_name': '\u0447\u0430\u0441',
-                'verbose_name_plural': '\u0447\u0430\u0441\u0443',
+                'verbose_name_plural': '\u0433\u043e\u0434\u0438\u043d\u0438',
             },
             bases=(models.Model,),
         ),
@@ -107,16 +115,19 @@ class Migration(migrations.Migration):
             name='Timetable',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('day', models.IntegerField(default=1, max_length=1, verbose_name=b'\xd0\x94\xd0\xb5\xd0\xbd\xd1\x8c', choices=[(1, b'\xd0\x9f\xd0\xbe\xd0\xbd\xd0\xb5\xd0\xb4\xd1\x96\xd0\xbb\xd0\xbe\xd0\xba'), (2, b'\xd0\x92\xd1\x96\xd0\xb2\xd1\x82\xd0\xbe\xd1\x80\xd0\xbe\xd0\xba'), (3, b'\xd0\xa1\xd0\xb5\xd1\x80\xd0\xb5\xd0\xb4\xd0\xb0'), (4, b'\xd0\xa7\xd0\xb5\xd1\x82\xd0\xb2\xd0\xb5\xd1\x80'), (5, b"\xd0\x9f'\xd1\x8f\xd1\x82\xd0\xbd\xd0\xb8\xd1\x86\xd1\x8f"), (6, b'\xd0\xa1\xd1\x83\xd0\xb1\xd0\xbe\xd1\x82\xd0\xb0'), (7, b'\xd0\x9d\xd0\xb5\xd0\xb4\xd1\x96\xd0\xbb\xd1\x8f')])),
+                ('subgroup', models.IntegerField(default=0, max_length=1, verbose_name=b'\xd0\x9f\xd1\x96\xd0\xb4\xd0\xb3\xd1\x80\xd1\x83\xd0\xbf\xd0\xb0', choices=[(0, b'\xd0\x97\xd0\xb0\xd0\xb3\xd0\xb0\xd0\xbb\xd1\x8c\xd0\xbd\xd0\xb0 \xd0\xb3\xd1\x80\xd1\x83\xd0\xbf\xd0\xb0'), (1, b'1 \xd0\xbf\xd1\x96\xd0\xb4\xd0\xb3\xd1\x80\xd1\x83\xd0\xbf\xd0\xb0'), (2, b'2 \xd0\xbf\xd1\x96\xd0\xb4\xd0\xb3\xd1\x80\xd1\x83\xd0\xbf\xd0\xb0'), (3, b'3 \xd0\xbf\xd1\x96\xd0\xb4\xd0\xb3\xd1\x80\xd1\x83\xd0\xbf\xd0\xb0')])),
+                ('day', models.IntegerField(default=0, max_length=1, verbose_name=b'\xd0\x94\xd0\xb5\xd0\xbd\xd1\x8c', choices=[(0, b'\xd0\x9f\xd0\xbe\xd0\xbd\xd0\xb5\xd0\xb4\xd1\x96\xd0\xbb\xd0\xbe\xd0\xba'), (1, b'\xd0\x92\xd1\x96\xd0\xb2\xd1\x82\xd0\xbe\xd1\x80\xd0\xbe\xd0\xba'), (2, b'\xd0\xa1\xd0\xb5\xd1\x80\xd0\xb5\xd0\xb4\xd0\xb0'), (3, b'\xd0\xa7\xd0\xb5\xd1\x82\xd0\xb2\xd0\xb5\xd1\x80'), (4, b"\xd0\x9f'\xd1\x8f\xd1\x82\xd0\xbd\xd0\xb8\xd1\x86\xd1\x8f"), (5, b'\xd0\xa1\xd1\x83\xd0\xb1\xd0\xbe\xd1\x82\xd0\xb0'), (6, b'\xd0\x9d\xd0\xb5\xd0\xb4\xd1\x96\xd0\xbb\xd1\x8f')])),
                 ('periodicity', models.IntegerField(default=0, max_length=1, verbose_name=b'\xd0\x9f\xd0\xb5\xd1\x80\xd1\x96\xd0\xbe\xd0\xb4\xd0\xb8\xd1\x87\xd0\xbd\xd1\x96\xd1\x81\xd1\x82\xd1\x8c', choices=[(0, b'\xd0\x97\xd0\xb0\xd0\xb2\xd0\xb6\xd0\xb4\xd0\xb8'), (1, b'\xd0\xa7\xd0\xb8\xd1\x81\xd0\xb5\xd0\xbb\xd1\x8c\xd0\xbd\xd0\xb8\xd0\xba'), (2, b'\xd0\x97\xd0\xbd\xd0\xb0\xd0\xbc\xd0\xb5\xd0\xbd\xd0\xbd\xd0\xb8\xd0\xba')])),
-                ('date_start', models.DateField(verbose_name=b'\xd0\x9f\xd0\xbe\xd1\x87\xd0\xb0\xd1\x82\xd0\xbe\xd0\xba \xd0\xbf\xd0\xb0\xd1\x80')),
-                ('date_end', models.DateField(verbose_name=b'\xd0\x9a\xd1\x96\xd0\xbd\xd0\xb5\xd1\x86\xd1\x8c \xd0\xbf\xd0\xb0\xd1\x80')),
+                ('date_start', models.DateField(default=datetime.date(2015, 2, 2), verbose_name=b'\xd0\x9f\xd0\xbe\xd1\x87\xd0\xb0\xd1\x82\xd0\xbe\xd0\xba \xd0\xbf\xd0\xb0\xd1\x80')),
+                ('date_end', models.DateField(default=datetime.date(2015, 5, 30), verbose_name=b'\xd0\x9a\xd1\x96\xd0\xbd\xd0\xb5\xd1\x86\xd1\x8c \xd0\xbf\xd0\xb0\xd1\x80')),
                 ('lesson_type', models.IntegerField(default=0, max_length=1, verbose_name=b'\xd0\xa2\xd0\xb8\xd0\xbf \xd0\xbf\xd1\x80\xd0\xb5\xd0\xb4\xd0\xbc\xd0\xb5\xd1\x82\xd1\x83', choices=[(0, b'\xd0\x9d\xd0\xb5 \xd0\xb7\xd0\xb0\xd0\xb4\xd0\xb0\xd0\xbd\xd0\xbe'), (1, b'\xd0\x9b\xd0\xb5\xd0\xba\xd1\x86\xd1\x96\xd1\x8f'), (2, b'\xd0\xa1\xd0\xb5\xd0\xbc\xd1\x96\xd0\xbd\xd0\xb0\xd1\x80'), (3, b'\xd0\x9b\xd0\xb0\xd0\xb1\xd0\xbe\xd1\x80\xd0\xb0\xd1\x82\xd0\xbe\xd1\x80\xd0\xbd\xd0\xb0'), (4, b'\xd0\x9a\xd0\xbe\xd0\xbd\xd1\x81\xd1\x83\xd0\xbb\xd1\x8c\xd1\x82\xd0\xb0\xd1\x86\xd1\x96\xd1\x8f'), (5, b'\xd0\x95\xd0\xba\xd0\xb7\xd0\xb0\xd0\xbc\xd0\xb5\xd0\xbd')])),
-                ('last_update', models.IntegerField(default=1420801485.4115)),
+                ('free_trajectory', models.BooleanField(default=False, verbose_name=b'\xd0\x92\xd1\x96\xd0\xbb\xd1\x8c\xd0\xbd\xd0\xb0 \xd1\x82\xd1\x80\xd0\xb0\xd0\xb5\xd0\xba\xd1\x82\xd0\xbe\xd1\x80\xd1\x96\xd1\x8f')),
+                ('last_update', models.IntegerField(default=1425733235)),
                 ('audience', models.ForeignKey(verbose_name=b'\xd0\x90\xd1\x83\xd0\xb4\xd0\xb8\xd1\x82\xd0\xbe\xd1\x80\xd1\x96\xd1\x8f', to='timetable.Audience')),
-                ('group', models.ForeignKey(verbose_name=b'\xd0\x93\xd1\x80\xd1\x83\xd0\xbf\xd0\xb0', to='timetable.Group')),
+                ('group', models.ManyToManyField(to='timetable.Group', verbose_name=b'\xd0\x93\xd1\x80\xd1\x83\xd0\xbf\xd0\xb0')),
                 ('lesson', models.ForeignKey(verbose_name=b'\xd0\x9f\xd1\x80\xd0\xb5\xd0\xb4\xd0\xbc\xd0\xb5\xd1\x82', to='timetable.Lesson')),
-                ('teacher', models.ForeignKey(verbose_name=b'\xd0\x92\xd0\xb8\xd0\xba\xd0\xbb\xd0\xb0\xd0\xb4\xd0\xb0\xd1\x87', to='timetable.Teacher', null=True)),
+                ('period', models.ForeignKey(verbose_name=b'\xd0\xa7\xd0\xb0\xd1\x81 \xd0\xbf\xd0\xb0\xd1\x80', to='timetable.Time', null=True)),
+                ('teacher', models.ForeignKey(verbose_name=b'\xd0\x92\xd0\xb8\xd0\xba\xd0\xbb\xd0\xb0\xd0\xb4\xd0\xb0\xd1\x87', to='timetable.Teacher')),
             ],
             options={
                 'verbose_name': '\u0440\u043e\u0437\u043a\u043b\u0430\u0434',
