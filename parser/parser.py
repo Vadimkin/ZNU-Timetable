5884410c6189a8e7aeec1bf17451fa3aa454a1aa
Vadim <mail@vadimklimenko.com>
2015-03-29 16:37:28 +0300

    Improve parser

--- parser/parser.py
+++ parser/parser.py
@@ -3,18 +3,77 @@
 import os
 import sys
 import django
-sys.path.append(os.path.dirname(os.path.dirname(os. path.abspath(__file__))) + '/settings.py')
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '/settings.py')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'znu.settings'
+django.setup()
 
 import xlrd
-from timetable.models import Timetable
+from timetable.models import Timetable, Group, Lesson, Audience, Teacher
 
 __author__ = 'vadim'
 
-filename = 'example.xlsx'
+filename = '2332.xls'
 
-rb = xlrd.open_workbook(os.path.dirname(os. path.abspath(__file__)) + '/data/' + filename,formatting_info=False)
+week_days = {
+    u"Понеділок": 0,
+    u"Вівторок": 1,
+    u"Середа": 2,
+    u"Четвер": 3,
+    u"П’ятниця": 4,
+    u"Субота": 5
+}
+
+numerator = {
+    u"Чисельник": 1,
+    u"Знаменник": 2
+}
+
+rb = xlrd.open_workbook(os.path.dirname(os.path.abspath(__file__)) + '/data/' + filename, formatting_info=False)
 sheet = rb.sheet_by_index(0)
-for rownum in range(sheet.nrows):
+
+main_info = {}
+main_info['faculty'] = sheet.row_values(1)[0]
+main_info['group'] = sheet.row_values(1)[1]
+main_info['course'] = int(sheet.row_values(1)[2])
+
+main_info['faculty'] = 3
+
+# if group not found, then create it
+group, created = Group.objects.get_or_create(department_id=main_info['faculty'], name=main_info['group'],
+                                             course=main_info['course'])
+main_info['group_id'] = group.id
+
+for rownum in range(3, sheet.nrows):
     row = sheet.row_values(rownum)
-    print(row)
+
+    lesson = {}
+
+    lesson['week_day'] = week_days[row[0]]
+    lesson['time'] = int(row[1])
+    lesson['periodicity'] = 0 if row[2] == "" else numerator[row[2]]
+    lesson['type'] = 0 if row[3] == "" else int(row[3])
+    lesson['name'] = row[4]
+    lesson['teacher'] = u"—" if row[5] == "" else row[5]
+    lesson['audience'] = u"—" if row[6] == "" else int(row[6])
+    lesson['campus'] = 9 if row[7] == "" else int(row[7])
+    lesson['subgroup'] = 0 if row[8] == "" else int(row[8])
+    lesson['free_trajectory'] = row[8]
+
+    lesson_object, created = Lesson.objects.get_or_create(name=lesson['name'])
+    lesson['name_id'] = lesson_object.id
+
+    audience_object, created = Audience.objects.get_or_create(campus_id=lesson['campus'], audience=lesson['audience'])
+    lesson['audience_id'] = audience_object.id
+
+    teacher_object, created = Teacher.objects.get_or_create(name=lesson['teacher'])
+    lesson['teacher_id'] = teacher_object.id
+
+    timetable, created = Timetable.objects.get_or_create(lesson_id=lesson['name_id'], day=lesson['week_day'],
+                                                         audience_id=lesson['audience_id'],
+                                                         periodicity=lesson['periodicity'], lesson_type=lesson['type'],
+                                                         teacher_id=lesson['teacher_id'], period_id=lesson['time'],
+                                                         subgroup=lesson['subgroup'],
+                                                         free_trajectory=lesson['free_trajectory'])
+
+    timetable.group.add(main_info['group_id'])
